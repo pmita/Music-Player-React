d6f0526bbda28697f2ffafec00c1dd3bb7d973fc
panos.mita2@outlook.com <pmita@dxc.com>
2021-05-01 00:10:22 +0100

    sass styling added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+//Import styles
+import './styles/app.scss';
 //Importing components
 import Player from './components/Player';
 import Song from './components/Song';
@@ -7,8 +9,9 @@ function App() {
   return (
     <div className="App">
       <h1>Music Player</h1>
-      <Player />
       <Song />
+      <Player />
+      
     </div>
   );
 }
